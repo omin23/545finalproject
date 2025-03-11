8364b2a6700da1347ea6055042abed578c7da607
Omkar nayak <omkaromkar5555@gmail.com>
2025-03-11 15:25:38 -0400

    mbi should be done

--- Geneformer_project/scripts/geneformer_mbi.py
+++ Geneformer_project/scripts/geneformer_mbi.py
@@ -1,6 +1,7 @@
 from datasets import Dataset, DatasetDict
-from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer
 import scanpy as sc
+import harmonypy as hm
 import datetime
 import pandas as pd
 import numpy as np
@@ -36,5 +37,40 @@ df = adata.obs[["gene_sequence"]]
 
 df["description"] = "This dataset contains gene sequences and binary labels."
 
-print(df.head())
+# normalize
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
 
+# Select highly variable genes
+sc.pp.highly_variable_genes(adata, n_top_genes=2000)
+adata = adata[:, adata.var["highly_variable"]]
+
+
+sc.pp.pca(adata, n_comps=50)
+adata.obsm["X_pca"] = hm.run_harmony(adata.obsm["X_pca"], adata.obs, "batch").Z_corr
+adata.write("integrated_dataset.h5ad")
+
+genes = np.array(adata.var_names)
+expr_matrix = adata.X 
+
+def tokenize_cell_expression(expression_row):
+    """Sort genes by expression level and create a gene sequence"""
+    sorted_gene_indices = np.argsort(-expression_row)  # Descending order
+    sorted_genes = genes[sorted_gene_indices]
+    return " ".join(sorted_genes)
+
+# Convert each cell into a tokenized sequence
+adata.obs["gene_sequence"] = [tokenize_cell_expression(expr_matrix[i, :]) for i in range(expr_matrix.shape[0])]
+
+# Save tokenized dataset
+adata.write("tokenized_integrated_dataset.h5ad")
+
+
+# TOKENIZER 
+tokenizer = AutoTokenizer.from_pretrained("theislab/geneformer")
+
+# Tokenize sequences
+adata.obs["tokenized"] = adata.obs["gene_sequence"].apply(lambda x: tokenizer(x, truncation=True, padding="max_length"))
+
+# Save processed dataset
+adata.write("final_geneformer_ready_dataset.h5ad")
