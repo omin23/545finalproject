368321e9bdbafba91167ef9097fb7fe11f6a597f
Omkar nayak <omkaromkar5555@gmail.com>
2025-03-11 12:39:50 -0400

    CC done

--- Geneformer_project/scripts/geneformer_CC.py
+++ Geneformer_project/scripts/geneformer_CC.py
@@ -19,7 +19,7 @@ output_dir = f"Geneformer_project/runs/{datestamp}"
 
 # get the data from the scanpy package
 adata = sc.read("Datasets/filtered_ms_adata.h5ad")
-print (adata)
+print(adata)
 
 
 # make the directory and catch any errors that may occur
@@ -33,8 +33,10 @@ except Exception as e:
 
 
 # set the classifier this is from the examples 
-filter_data_dict={"cell_type":["Cardiomyocyte1","Cardiomyocyte2","Cardiomyocyte3"]}
+filter_data_dict={"cell_type":["oligodendrocyte A","oligodendrocyte precursor cell","oligodendrocyte C"]}
 
+
+# set the training arguments
 training_args = {
     "num_train_epochs": 0.9,
     "learning_rate": 0.000804,
@@ -45,17 +47,43 @@ training_args = {
     "seed": 73,
 }
 
+# set the classifier
 cc = Classifier(classifier="cell",
-                cell_state_dict = {"state_key": "disease", "states": "all"},
+                cell_state_dict={"state_key": "Sample Characteristic Ontology Term[disease]", "states": "Sample Characteristic Ontology Term[organism status]"},
                 filter_data=filter_data_dict,
                 training_args=training_args,
                 max_ncells=None,
-                freeze_layers = 2,
-                num_crossval_splits = 1,
+                freeze_layers=2,
+                num_crossval_splits=1,
                 forward_batch_size=200,
-                nproc=16)
+                nproc=8)
 
+train_ids = ["1447", "1600", "1462", "1558", "1300", "1508", "1358", "1678", "1561", "1304", "1610", "1430", "1472", "1707", "1726", "1504", "1425", "1617", "1631", "1735", "1582", "1722", "1622", "1630", "1290", "1479", "1371", "1549", "1515"]
+eval_ids = ["1422", "1510", "1539", "1606", "1702"]
+test_ids = ["1437", "1516", "1602", "1685", "1718"]
 
+train_test_id_split_dict = {
+    "attr_key": "individual",
+    "train": train_ids + eval_ids,
+    "test": test_ids
+}
 
+# Example input_data_file for 30M model: https://huggingface.co/datasets/ctheodoris/Genecorpus-30M/tree/main/example_input_files/cell_classification/disease_classification/human_dcm_hcm_nf.dataset
+cc.prepare_data(input_data_file="Datasets/filtered_ms_adata.h5ad",
+                output_directory=output_dir,
+                output_prefix=output_prefix,
+                split_id_dict=train_test_id_split_dict)
+
+train_valid_id_split_dict = {"attr_key": "individual",
+                            "train": train_ids,
+                            "eval": eval_ids}
+
+all_metrics = cc.validate(model_directory="/path/to/Geneformer",
+                          prepared_input_data_file=f"{output_dir}/{output_prefix}_labeled_train.dataset",
+                          id_class_dict_file=f"{output_dir}/{output_prefix}_id_class_dict.pkl",
+                          output_directory=output_dir,
+                          output_prefix=output_prefix,
+                          split_id_dict=train_valid_id_split_dict,
+                          n_hyperopt_trials=100)  # to optimize hyperparameters, set n_hyperopt_trials=100 (or alternative desired # of trials)
 
 
